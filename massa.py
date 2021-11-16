7d224670f8b5b671cdcbffb55915acec22b00f66
Voland <54623455+cheverev@users.noreply.github.com>
2021-11-16 12:13:44 +0200

    Update massa.py

--- massa.py
+++ massa.py
@@ -1,22 +1,18 @@
-import urllib
+from jsonrpcclient import request
 import requests
-import json
+import os
 import time
 
-telegram_chat = "****"
-bot_token = "*****"
-send_text =  "********"
-massa_addr = "****"
-send_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={telegram_chat}&text={send_text}"
-
-
-url = f"http://127.0.0.1:33033/api/v1/addresses_info?addrs[0]={massa_addr}"
-res = urllib.request.urlopen(url).read()
-js = json.loads(res)
-rolls = js.get(massa_addr).get("active_rolls")
+address = "iPwUxhRdcZB16FksEv5K8Xe2LFTWDMtAYwdjQZLMCEAWEvbCr"
+res = requests.post("http://127.0.0.1:33035/", json=request("get_addresses",params=[[address]]))
+massa = int(float(res.json().get("result")[0].get("balance").get("final_balance")))
 
 while True:
-    if rolls == 0:
-        requests.get(send_url)
+    if(massa // 100 > 0 ):
+        os.system("date >> /root/massa.log")
+        count = massa // 100
+        os.system(f"cd /root/massa/massa-client && /root/massa/target/release/massa-client --wallet wallet.dat buy_rolls {address} {count} 0 >> /root/massa.log")
+        time.sleep(60)
     else:
+        os.system("date >> /root/massa.log")
         time.sleep(60)
