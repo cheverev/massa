b4f124338496b35c43f1f9ad71dbed1af446d379
Voland <54623455+cheverev@users.noreply.github.com>
2021-11-21 10:53:00 +0200

    Update massa.py
    add try except

--- massa.py
+++ massa.py
@@ -6,10 +6,14 @@ import time
 address = "iPwUxhRdcZB16FksEv5K8Xe2LFTWDMtAYwdjQZLMCEAWEvbCr"
 
 while True:
-    res = requests.post("http://127.0.0.1:33035/", json=request("get_addresses",params=[[address]]))
-    massa = int(float(res.json().get("result")[0].get("balance").get("final_balance")))
-    if(massa // 100 > 0 ):
-        os.system("date >> /root/massa.log")
-        count = massa // 100
-        os.system(f"cd /root/massa/massa-client && /root/massa/target/release/massa-client --wallet wallet.dat buy_rolls {address} {count} 0 >> /root/massa.log")
-    time.sleep(60)
+   try:
+        res = requests.post("http://127.0.0.1:33035/", json=request("get_addresses",params=[[address]]))
+        massa = int(float(res.json().get("result")[0].get("balance").get("final_balance")))
+        if(massa // 100 > 0 ):
+            os.system("date >> /root/massa.log")
+            count = massa // 100
+            os.system(f"cd /root/massa/massa-client && /root/massa/target/release/massa-client --wallet wallet.dat buy_rolls {address} {count} 0 >> /root/massa.log")
+        time.sleep(60)
+    except Exception as e:
+        print(e)
+        time.sleep(60)
