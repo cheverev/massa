1777037ef0386e99563cb68b43dae60ebcfe3dc6
Voland <54623455+cheverev@users.noreply.github.com>
2021-12-02 20:47:55 +0200

    Update massa.py
    update massa balance

--- massa.py
+++ massa.py
@@ -8,7 +8,7 @@ address = "iPwUxhRdcZB16FksEv5K8Xe2LFTWDMtAYwdjQZLMCEAWEvbCr"
 while True:
     try:
         res = requests.post("http://127.0.0.1:33035/", json=request("get_addresses",params=[[address]]))
-        massa = int(float(res.json().get("result")[0].get("balance").get("final_balance")))
+        massa =  int(float(res.json().get("result")[0].get("ledger_info").get("final_ledger_info").get("balance")))
         if(massa // 100 > 0 ):
             os.system("date >> /root/massa.log")
             count = massa // 100
