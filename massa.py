6cef8b432ea6f9b069eb362f02d891f47937e455
Voland <54623455+cheverev@users.noreply.github.com>
2022-05-07 14:10:02 +0300

    Update massa.py

--- massa.py
+++ massa.py
@@ -9,11 +9,15 @@ while True:
     try:
         res = requests.post("http://127.0.0.1:33035/", json=request("get_addresses",params=[[address]]))
         massa =  int(float(res.json().get("result")[0].get("ledger_info").get("final_ledger_info").get("balance")))
+        ActiveRolls=int(res.json().get("result")[0].get("rolls").get("active_rolls"))
+        CandidateRolls=int(res.json().get("result")[0].get("rolls").get("candidate_rolls"))
+        if(ActiveRolls >= 1 or CandidateRolls == 1):
+            break
         if(massa // 100 > 0 ):
             os.system("date >> /root/massa.log")
             count = massa // 100
             os.system(f"cd /root/massa/massa-client && /root/massa/target/release/massa-client --wallet wallet.dat buy_rolls {address} {count} 0 >> /root/massa.log")
-        time.sleep(60)
+        time.sleep(300)
     except Exception as e:
         print(e)
         time.sleep(60)
