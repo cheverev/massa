91e40092677aba1b39c5fe0c006634e2eb48507f
Voland <54623455+cheverev@users.noreply.github.com>
2022-05-07 15:59:57 +0300

    Update massa.py

--- massa.py
+++ massa.py
@@ -6,18 +6,15 @@ import time
 address = "iPwUxhRdcZB16FksEv5K8Xe2LFTWDMtAYwdjQZLMCEAWEvbCr"
 
 while True:
-    try:
+    try:https://github.com/cheverev/massa/blob/main/massa.py
         res = requests.post("http://127.0.0.1:33035/", json=request("get_addresses",params=[[address]]))
-        massa = int(float(res.json().get("result")[0].get("ledger_info").get("final_ledger_info").get("balance")))
         ActiveRolls = int(res.json().get("result")[0].get("rolls").get("active_rolls"))
         CandidateRolls = int(res.json().get("result")[0].get("rolls").get("candidate_rolls"))
         if(ActiveRolls >= 1 or CandidateRolls == 1):
             break
-        if(massa // 100 > 0 ):
-            os.system("date >> /root/massa.log")
-            count = massa // 100
-            os.system(f"cd /root/massa/massa-client && /root/massa/target/release/massa-client --wallet wallet.dat buy_rolls {address} {count} 0 >> /root/massa.log")
+        os.system("date >> /root/massa.log")
+        os.system(f"cd /root/massa/massa-client && /root/massa/target/release/massa-client --wallet wallet.dat buy_rolls {address} 1 0 >> /root/massa.log")
         time.sleep(300)
     except Exception as e:
         print(e)
-        time.sleep(60)
+        time.sleep(300)
